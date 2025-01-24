b8ae83f1db3b5f16cffb5fec828c70c9606aed84
Garlan Ngoune Nguetsa <garloon30@gmail.com>
2025-01-24 15:15:24 +0100

    Exemple avec carré

--- L'Echo des Larmes UNITY/Assets/Scripts/MouseOvering.cs
+++ L'Echo des Larmes UNITY/Assets/Scripts/MouseOvering.cs
@@ -4,23 +4,27 @@ using UnityEngine.Rendering;
 public class MouseOvering : MonoBehaviour
 {
     GameObject Indice;
+    GameObject Indice_child;
     SpriteRenderer sprite;
     Color MouseOverColor = Color.yellow;
     Color OriginalColor;
     void Start()
     {
         Indice = GetComponent<GameObject>();
+        Indice_child = Indice.transform.GetChild(0).gameObject;
         sprite = GetComponent<SpriteRenderer>();
         OriginalColor = sprite.material.color;
     }
     void OnMouseOver()
     {
         sprite.material.color = MouseOverColor;
+        Indice_child.SetActive(true);
         Debug.Log("La souris est dessus");
     }
 
     void OnMouseExit()
     {
         sprite.material.color = OriginalColor;
+        Indice_child.SetActive(false);
     }
 }
