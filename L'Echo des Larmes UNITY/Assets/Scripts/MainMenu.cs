b151dab9a9dc0c907d659f06860f9f2555806c2f
Paul Godek <paul.godek30@gmail.com>
2025-01-24 12:46:56 +0100

    test timeline

--- L'Echo des Larmes UNITY/Assets/Scripts/MainMenu.cs
+++ L'Echo des Larmes UNITY/Assets/Scripts/MainMenu.cs
@@ -3,8 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void startGame() {
+    public void StartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    
-    }
+    }   
 }
