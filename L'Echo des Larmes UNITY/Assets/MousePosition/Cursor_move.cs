7a689c4a1e9e1eaf61af1bd6b5f9acd0e7ffcd0a
Garlan Ngoune Nguetsa <garloon30@gmail.com>
2025-01-24 13:32:43 +0100

    Cursor mouvement complete

--- L'Echo des Larmes UNITY/Assets/MousePosition/Cursor_move.cs
+++ L'Echo des Larmes UNITY/Assets/MousePosition/Cursor_move.cs
@@ -1,7 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
+<<<<<<< Updated upstream
+=======
+using static UnityEngine.Vector3;
+>>>>>>> Stashed changes
 
 public class Cursor_move : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject Souris;
+    Vector3 MousePosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,12 @@ public class Cursor_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
 
+=======
+        MousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        MousePosition.z = 0f;
+        transform.position = MousePosition;
+>>>>>>> Stashed changes
     }
 }
