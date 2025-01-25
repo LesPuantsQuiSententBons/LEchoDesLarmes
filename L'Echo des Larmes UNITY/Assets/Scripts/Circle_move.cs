eddb62da39f34d70371293dd8c3956ebdb0b1abc
Garlan Ngoune Nguetsa <garloon30@gmail.com>
2025-01-25 04:10:43 +0100

    Succession de l'enigme de l'horloge

--- L'Echo des Larmes UNITY/Assets/Scripts/Circle_move.cs
+++ L'Echo des Larmes UNITY/Assets/Scripts/Circle_move.cs
@@ -1,31 +1,60 @@
+using System.Transactions;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Circle_move : MonoBehaviour
 {
-    Vector3 Cercle_position;
-    void OnMouseDown()
+    GameObject Curseur;
+    PolygonCollider2D polygon1,polygon2,polygon3;
+    void Start()
     {
-        //Cercle_position = circle_Start.Rouage[0].transform.position;
-        //Cercle_position.x = Cercle_position.y = 0;
-        //Debug.Log("Sur Rouage1");
+        Curseur = GameObject.FindGameObjectWithTag("Curseur");
+        polygon1 = GameObject.Find("Rouage1").GetComponent<PolygonCollider2D>();
+        polygon2 = GameObject.Find("Rouage2").GetComponent<PolygonCollider2D>();
+        polygon3 = GameObject.Find("Rouage3").GetComponent<PolygonCollider2D>();
     }
-
     void OnMouseOver()
     {
-        switch(this.name)
+        Vector3 Cursor_look;
+        float Angle;
+
+        if(Input.GetMouseButton(0))
         {
+            switch(name)
+            {
             case "Rouage1":
-                Debug.Log("Sur Rouage1");
+                polygon2.enabled = false;
+                polygon3.enabled = false;
+                Cursor_look = transform.InverseTransformDirection(Curseur.transform.position);
+                Angle = Mathf.Atan2(Cursor_look.y,Cursor_look.x) * Mathf.Rad2Deg - 90;
+                transform.Rotate(0,0,Angle);
                 break;
             case "Rouage2":
-                Debug.Log("Sur Rouage2");
+                polygon1.enabled = false;
+                polygon3.enabled = false;
+                Cursor_look = transform.InverseTransformDirection(Curseur.transform.position);
+                Angle = Mathf.Atan2(Cursor_look.y,Cursor_look.x) * Mathf.Rad2Deg - 90;
+                transform.Rotate(0,0,Angle);
                 break;
             case "Rouage3":
-                Debug.Log("Sur Rouage3");
+                polygon1.enabled = false;
+                polygon2.enabled = false;
+                Cursor_look = transform.InverseTransformDirection(Curseur.transform.position);
+                Angle = Mathf.Atan2(Cursor_look.y,Cursor_look.x) * Mathf.Rad2Deg - 90;
+                transform.Rotate(0,0,Angle);
                 break;
             default:
-                Debug.Log("Problème");
-                break;
+                    Debug.Log("Y'A PAS D'AUTRES ROUAGES CHEF");
+                    break;
+            }
+        }
+        else
+        {
+            polygon1.enabled = true;
+            polygon2.enabled = true;
+            polygon3.enabled = true;
         }
     }
+            
 }
