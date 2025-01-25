42e5cf88927dd1a50661617ac5e46e471b3792b3
Garlan Ngoune Nguetsa <garloon30@gmail.com>
2025-01-25 05:43:53 +0100

    Changement de Wheel Enigma (encore) + build project

--- L'Echo des Larmes UNITY/Assets/Scripts/Circle_move.cs
+++ L'Echo des Larmes UNITY/Assets/Scripts/Circle_move.cs
@@ -4,18 +4,26 @@ using UnityEngine.InputSystem;
 
 public class Circle_move : MonoBehaviour
 {
-    GameObject Curseur,rouage1,rouage2,rouage3;
+    GameObject Curseur;
+    public GameObject rouage1,rouage2,rouage3;
     PolygonCollider2D polygon1,polygon2,polygon3;
     void Start()
     {
         Curseur = GameObject.FindGameObjectWithTag("Curseur");
-        rouage1 = GameObject.Find("Rouage1").GetComponent<GameObject>();
-        rouage2 = GameObject.Find("Rouage2").GetComponent<GameObject>();
-        rouage3 = GameObject.Find("Rouage3").GetComponent<GameObject>();
         polygon1 = GameObject.Find("Rouage1").GetComponent<PolygonCollider2D>();
         polygon2 = GameObject.Find("Rouage2").GetComponent<PolygonCollider2D>();
         polygon3 = GameObject.Find("Rouage3").GetComponent<PolygonCollider2D>();
         transform.Rotate(0,0,Random.value * 360);
+    }
+
+    void Update()
+    {
+       /* if(isRouage1True)
+            Debug.Log("Grosse roue résolue");
+        if(rouage1.transform.rotation.z > 120)
+            Debug.Log("Au-dessus de 120");*/
+        if(rouage1.transform.rotation.z > rouage1.transform.rotation.z+0.1*0.92)
+            Debug.Log("Rouage 1 supérieur");
     }
     void OnMouseOver()
     {
@@ -57,11 +65,16 @@ public class Circle_move : MonoBehaviour
             polygon1.enabled = true;
             polygon2.enabled = true;
             polygon3.enabled = true;
-            if((rouage1.transform.rotation.z > 130 && 140 > rouage1.transform.rotation.z)||(rouage1.transform.rotation.z > 130 && 140 > rouage1.transform.rotation.z))
+            Debug.Log("Rouage 1 en z est : " + rouage1.transform.rotation.z);
+            Debug.Log("Rouage 2 en z est : " + rouage2.transform.rotation.z);
+            Debug.Log("Rouage 3 en z est : " + rouage3.transform.rotation.z);
+            if(-0.85 < rouage1.transform.rotation.z && rouage1.transform.rotation.z < -0.95)
                 {
-                    if((rouage2.transform.rotation.z > 130 && 140 > rouage2.transform.rotation.z) || (rouage2.transform.rotation.z > 130 && 140 > rouage2.transform.rotation.z))
+                    Debug.Log("Bonne réponse");
+                    if(-0.85 < rouage2.transform.rotation.z && rouage2.transform.rotation.z < -0.95)
                         {
-                            if((rouage3.transform.rotation.z > -40 && -50 > rouage3.transform.rotation.z) || (rouage3.transform.rotation.z > -40+180 && -50+180 > rouage3.transform.rotation.z))
+                            Debug.Log("2 Bonne réponse");
+                            if(-50 < rouage3.transform.rotation.z && rouage3.transform.rotation.z < -40)
                             Debug.Log("Enigme Résolu");
                         }
                 }
