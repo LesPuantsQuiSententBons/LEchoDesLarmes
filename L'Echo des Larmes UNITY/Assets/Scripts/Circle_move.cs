4eba4ebacc5280a35f8806f0c2c03962302b5959
Garlan Ngoune Nguetsa <garloon30@gmail.com>
2025-01-25 04:47:19 +0100

    Change of Wheel Enigma

--- L'Echo des Larmes UNITY/Assets/Scripts/Circle_move.cs
+++ L'Echo des Larmes UNITY/Assets/Scripts/Circle_move.cs
@@ -1,18 +1,21 @@
-using System.Transactions;
-using Unity.Mathematics;
 using UnityEngine;
+using static UnityEngine.Random;
 using UnityEngine.InputSystem;
 
 public class Circle_move : MonoBehaviour
 {
-    GameObject Curseur;
+    GameObject Curseur,rouage1,rouage2,rouage3;
     PolygonCollider2D polygon1,polygon2,polygon3;
     void Start()
     {
         Curseur = GameObject.FindGameObjectWithTag("Curseur");
+        rouage1 = GameObject.Find("Rouage1").GetComponent<GameObject>();
+        rouage2 = GameObject.Find("Rouage2").GetComponent<GameObject>();
+        rouage3 = GameObject.Find("Rouage3").GetComponent<GameObject>();
         polygon1 = GameObject.Find("Rouage1").GetComponent<PolygonCollider2D>();
         polygon2 = GameObject.Find("Rouage2").GetComponent<PolygonCollider2D>();
         polygon3 = GameObject.Find("Rouage3").GetComponent<PolygonCollider2D>();
+        transform.Rotate(0,0,Random.value * 360);
     }
     void OnMouseOver()
     {
@@ -54,6 +57,14 @@ public class Circle_move : MonoBehaviour
             polygon1.enabled = true;
             polygon2.enabled = true;
             polygon3.enabled = true;
+            if((rouage1.transform.rotation.z > 130 && 140 > rouage1.transform.rotation.z)||(rouage1.transform.rotation.z > 130 && 140 > rouage1.transform.rotation.z))
+                {
+                    if((rouage2.transform.rotation.z > 130 && 140 > rouage2.transform.rotation.z) || (rouage2.transform.rotation.z > 130 && 140 > rouage2.transform.rotation.z))
+                        {
+                            if((rouage3.transform.rotation.z > -40 && -50 > rouage3.transform.rotation.z) || (rouage3.transform.rotation.z > -40+180 && -50+180 > rouage3.transform.rotation.z))
+                            Debug.Log("Enigme Résolu");
+                        }
+                }
         }
     }
             
